35eb230e0aa22991e6a5a01e084be4b7ce345548
Christian Giacomi <christiangiacomi@gmail.com>
2011-05-05 23:26:16 +0200

    Cleaned up the code

--- sms2droid.rb
+++ sms2droid.rb
@@ -23,59 +23,59 @@ require 'cgi'
 #model class representing a sent or received sms
 #
 class Message
-	attr_accessor :number, :text, :date, :flags
-	
-	def initialize(num, txt, dt, flgs)
-	  @number = num
-	  @text = txt
-	  @date = dt
-	  @flags = flgs
-	end
-	
-	#how each message should be represented in xml
-	def to_xml
-	  "\t<sms protocol=\"0\" address=\"#{number}\" date=\"#{date}000\" type=\"#{flags-1}\" subject=\"null\" body=\"#{CGI::escapeHTML("#{text}")}\" toa=\"0\" sc_toa=\"0\" service_center=\"null\" read=\"1\" status=\"-1\" locked=\"0\" contact_name=\"null\" />"
-	end
+  attr_accessor :number, :text, :date, :flags
+
+  def initialize(num, txt, dt, flgs)
+    @number = num
+    @text = txt
+    @date = dt
+    @flags = flgs
+  end
+
+  #how each message should be represented in xml
+  def to_xml
+    "\t<sms protocol=\"0\" address=\"#{number}\" date=\"#{date}000\" type=\"#{flags-1}\" subject=\"null\" body=\"#{CGI::escapeHTML("#{text}")}\" toa=\"0\" sc_toa=\"0\" service_center=\"null\" read=\"1\" status=\"-1\" locked=\"0\" contact_name=\"null\" />"
+  end
 end
 
 #
 #write the messages to the xml file
 #
 def write_to_file(msgs)
-	File.open('sms.xml', 'w') do |file| 
-		
-		#add the header to the xml file
-		file.puts "<?xml version='1.0' encoding='UTF-8' standalone='yes' ?>"
-		file.puts "<smses count=\"#{msgs.length}\">"
-	
-		#iterate over the messages and write the content   
-		msgs.each do |msg|
-			file.puts msg.to_xml
-		end
-		
-		#add the footer
-		file.puts "</smses>"
-	end  
+  File.open('sms.xml', 'w') do |file| 
+
+    #add the header to the xml file
+    file.puts "<?xml version='1.0' encoding='UTF-8' standalone='yes' ?>"
+    file.puts "<smses count=\"#{msgs.length}\">"
+
+    #iterate over the messages and write the content   
+    msgs.each do |msg|
+      file.puts msg.to_xml
+    end
+
+    #add the footer
+    file.puts "</smses>"
+  end  
 end
 
 #
 #extracts the values from the DB
 #
 def extract_from_db(dbfilepath)	
-	collection = Array.new
-	
-	db = SQLite3::Database.new("#{dbfilepath}")
+  collection = Array.new
+
+  db = SQLite3::Database.new("#{dbfilepath}")
   db.results_as_hash = true #otherwise you cannot referr to the columns via the column name!
   db.execute( "select * from message" ) do |row|	  
     num = row['address']
     txt = row['text']
     dt = row['date']
     flgs = row['flags'] 
-       
-  	collection.push(Message.new(num, txt, dt, flgs))
-  end	 
-	
-	return collection
+
+    collection.push(Message.new(num, txt, dt, flgs))
+  end
+
+  return collection
 end
 
 def done(count)
@@ -97,7 +97,7 @@ end
 
 
 #
-#DO THE PROCESSING!
+#DO THE WORK!
 #
 puts("\nsms2droid")
 puts("path to sms.db:")
@@ -106,8 +106,8 @@ file_to_convert = gets.chomp
 if File.exists?(file_to_convert)
   begin
     messages = extract_from_db(file_to_convert)
-  	write_to_file(messages)	
-  	done(messages.length)
+    write_to_file(messages)	
+    done(messages.length)
   rescue Exception => e
     exception
   end    
